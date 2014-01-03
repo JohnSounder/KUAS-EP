8d652c030040b1438494916d05b394fe4e3a6249
JohnSounder <wnz9u3lb@gmail.com>
2014-01-03 19:12:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 
 import requests
 from lxml import etree
-import login
+import login,submit
 
 if __name__ == '__main__':
-    if login.Login():
-        response = requests.get(login.ACTIVE_URL , headers = login.headers)
-        root = etree.HTML(response.text)
-    
-        for i in root.xpath("//a[starts-with(@id, 'ContentPlaceHolder1_ContentPlaceHolder1_TabContainer1_PopularPanel_gvPopular_LBact_name_')]"):
-            print i.text
+    print submit.Search()
+   # if login.Login():
+   #     response = requests.get(login.ACTIVE_URL , headers = login.headers)
+
       

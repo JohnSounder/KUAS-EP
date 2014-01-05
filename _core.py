e663ffb186c884f98bf9476c7af9f56f0f088554
JohnSounder <wnz9u3lb@gmail.com>
2014-01-05 23:06:34 +0800

    core_url_add

--- _core.py
+++ _core.py
@@ -1,1 +1,22 @@
+import time
+import re
+import collections
+import requests
+import uniout
+from lxml import etree
+import json
+
 CONST_VALUE_TEST = 10
+
+MAIN_URL = "http://ep.kuas.edu.tw/"
+POST_URL = "http://ep.kuas.edu.tw/EPortfolio/EPDefaultPage.aspx"
+ACTIVE_URL="http://ep.kuas.edu.tw/EPortfolio/Activity/ActivitySystem.aspx"
+
+
+headers = {}
+headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0"
+headers['Accept'] = "zh-tw,en-us;q=0.7,en;q=0.3"
+headers['Accept-Encoding'] = "gzip, deflate"
+headers['Referer'] = "http://ep.kuas.edu.tw/EPortfolio/EPDefaultPage.aspx"
+headers['Connection'] = "keep-alive"
+Dict={}

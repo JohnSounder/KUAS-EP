827f17e3e7ac08de1ce15a501dec47135fdc39e6
JohnSounder <wnz9u3lb@gmail.com>
2014-01-05 23:15:40 +0800

    11:15

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import _core
 import requests
 from lxml import etree
 import login,submit
 
 if __name__ == '__main__':
     print submit.Search()
-   # if login.Login():
-   #     response = requests.get(login.ACTIVE_URL , headers = login.headers)
+
+    #if login.Login():
+    #    response = requests.get(_core.ACTIVE_URL , headers = _core.headers)
 
       
